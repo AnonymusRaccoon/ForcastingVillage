21af9ed889ab7b9a132db21cd71e4d6b64eabce9
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-03-31 14:47:50 +0200

    Finishing collisions with the map

--- src/systems/map_movement_system.c
+++ src/systems/map_movement_system.c
@@ -6,8 +6,8 @@
 */
 
 
-#include <components/controllable_component.h>
-#include <components/transform_component.h>
+#include "components/controllable_component.h"
+#include "components/map_linker.h"
 #include "map_utils.h"
 #include "system.h"
 
@@ -16,15 +16,23 @@ static void update_entity(gc_engine *engine, void *system, gc_entity *entity, \
 float dtime)
 {
     struct controllable_component *ctl = GETCMP(entity, controllable_component);
-    struct transform_component *tra = GETCMP(entity, transform_component);
-    gc_vector2 move = gc_vector2_from_coords(ctl->movement_x, ctl->movement_y, 0);
-    struct vertex_component *map = GETSYS(engine, vertex_component);
-    gc_vector2 new_pos = gc_vector2_add(tra->position, move);
-    struct tile *new_tile = get_tile_from_pos(map, new_pos);
+    struct map_linker *link = GETCMP(entity, map_linker);
+    gc_scene *scene = engine->scene;
+    gc_list *maps = scene->get_entity_by_cmp(scene, "vertex_component");
+    struct vertex_component *map;
+    gc_vector2i map_pos = (gc_vector2i){
+        link->tile->corners[0]->x + ctl->movement_x,
+        link->tile->corners[0]->y + ctl->movement_y
+    };
+    struct tile *new_tile;
 
+    if (!maps)
+        return;
+    map = GETCMP(maps->data, vertex_component);
+    new_tile = get_tile_at(map, map_pos);
     if (new_tile && !new_tile->solid && ctl->move_callback <= 0) {
-        tra->position.x += move.x;
-        tra->position.y += move.y;
+        link->tile->entity = NULL;
+        new_tile->entity = entity;
         ctl->move_callback = 10;
     }
     ctl->move_callback--;
