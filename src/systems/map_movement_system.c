7467e03b15e6ac380f3722ed17440610bdd55971
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-03-31 12:29:55 +0200

    Really placing the player on the map

--- src/systems/map_movement_system.c
+++ src/systems/map_movement_system.c
@@ -18,8 +18,11 @@ float dtime)
     struct controllable_component *ctl = GETCMP(entity, controllable_component);
     struct transform_component *tra = GETCMP(entity, transform_component);
     gc_vector2 move = gc_vector2_from_coords(ctl->movement_x, ctl->movement_y, 0);
+    struct vertex_component *map = GETSYS(engine, vertex_component);
+    gc_vector2 new_pos = gc_vector2_add(tra->position, move);
+    struct tile *new_tile = get_tile_from_pos(map, new_pos);
 
-    if (ctl->move_callback <= 0) {
+    if (new_tile && !new_tile->solid && ctl->move_callback <= 0) {
         tra->position.x += move.x;
         tra->position.y += move.y;
         ctl->move_callback = 10;
