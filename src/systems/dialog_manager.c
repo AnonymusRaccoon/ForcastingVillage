663b898d93ce0514b5d8b3b06fb18302e63c0e2f
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-04-01 17:32:23 +0200

    Adding a basic dialog manager but text is still hard coded

--- src/systems/dialog_manager.c
+++ src/systems/dialog_manager.c
@@ -5,11 +5,22 @@
 ** dialog_manager.c
 */
 
+#include <components/map_linker.h>
 #include "my.h"
 #include "components/dialog_holder.h"
 #include "components/tag_component.h"
 #include "prefab.h"
 #include "engine.h"
+#include "tile.h"
+
+void load_dialog(gc_engine *engine, gc_entity *player)
+{
+    struct map_linker *link = GETCMP(player, map_linker);
+
+    if (!link || !link->tile->type || my_strcmp(link->tile->type, "dialog"))
+        return;
+    prefab_load(engine, "prefabs/dialog.gcprefab");
+}
 
 static void check_for_dialog(gc_engine *engine, va_list args)
 {
@@ -17,13 +28,13 @@ static void check_for_dialog(gc_engine *engine, va_list args)
     gc_keybindings key = va_arg(args, gc_keybindings);
     gc_scene *scene = engine->scene;
     gc_list *list;
+    gc_entity *entity = scene->get_entity(scene, 50);
 
-    if (key != SPACE)
+    if (key != SPACE || !entity)
         return;
-
     dima->is_dialog_open = !dima->is_dialog_open;
     if (dima->is_dialog_open) {
-        prefab_load(engine, "prefabs/dialog.gcprefab");
+        load_dialog(engine, entity);
         return;
     }
     list = scene->get_entity_by_cmp(scene, "tag_component");
