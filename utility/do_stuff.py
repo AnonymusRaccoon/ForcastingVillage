1d8f13aee11ffd546e67082e7bdecb17ef311498
Clément Le Bihan <clement.le-bihan@epitech.eu>
2020-04-30 00:20:41 +0200

    adding all walls of the maps

--- utility/do_stuff.py
+++ utility/do_stuff.py
@@ -3,19 +3,17 @@ import sys, xml.etree.ElementTree as ET
 
 try:
     file_entity = sys.argv[1]
-    elements = sys.argv[2:]
 except IndexError:
     print(f"Usage: {sys.argv[0]} file entity")
     sys.exit(1)
 tree = ET.parse(file_entity)
 root = tree.getroot()
-for entity in root.findall("gc_entity"):
-    attribs = entity.find('map_linker').attrib
-    attribs['name'] = entity.find("renderer").get("src")
-    #print(f"x:{attribs['x']} y:{attribs['y']} name:{attribs['name']}")
-    if not (attribs['name'] in elements):
+for entity in root.findall("./tiles_data/tile"):
+    attribs = entity.attrib
+    #print(attribs['x'])
+    if (int(attribs['y']) > 16):
         continue
-    attribs['x'] = str(int(attribs['x']) - 1)
-    attribs['y'] = str(int(attribs['y']) + 1)
+    #print(f"x:{attribs['x']} y:{attribs['y']} name:{attribs['name']}")
+    attribs['solid'] = "true"
 
 tree.write(file_entity)
