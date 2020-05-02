974590c9f2646dc634687522f0d20732381769f9
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-05-02 14:18:01 +0200

    The lumberjack now gives the shield upgrade

--- src/npc/lumberjack.c
+++ src/npc/lumberjack.c
@@ -9,17 +9,26 @@
 #include <components/dialog_holder.h>
 #include "engine.h"
 
+void lumberjack_delete(gc_engine *engine)
+{
+    struct dialog_holder *dialog = dialog_get_current(engine);
+
+    dialog->text[3] = NULL;
+}
+
 bool lumberjack_yes(gc_engine *engine, gc_entity *entity, \
 gc_vector2 pos, enum gc_mousekeys key)
 {
     gc_entity *player = engine->scene->get_entity(engine->scene, 50);
     struct dialog_holder *dialog = dialog_get_current(engine);
+    struct dialog_line *di;
     struct player_component *inv;
 
     if (!player)
         return (false);
     inv = GETCMP(player, player_component);
     inv->inventory_upgrades[3] = true;
-//    dialog->
+    di = dialog_add_line(dialog, NULL, "You gained the shield upgrade!", NULL);
+    di->callback = &lumberjack_delete;
     return (true);
 }
